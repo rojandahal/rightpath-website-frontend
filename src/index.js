8b534061acb61fd75564babdf21ad94cc23ee334
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-07-09 22:40:19 +0545

    feat: added cookies Provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { CookiesProvider } from 'react-cookie'
 
 const queryClient = new QueryClient()
 
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
     </QueryClientProvider>
   </React.StrictMode>
 )
