9a2430f80af3939b85f42248a591d2ba79242e00
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-07-10 11:52:50 +0545

    refactor: app.js and imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./App.css";
-import Home from "./container/Home";
-import AdminPage from "./container/Admin/AdminPage";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import AdminPageHome from "./container/Admin/AdminPageHome";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Home from './container/Home'
+import AdminLoginPage from './container/Admin/Login/Login'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import AdminHomePage from './container/Admin/Home/Home'
 
-function Main () {
+function Main() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/rightpath/admin" element={<AdminPage/>}></Route>
-      <Route path="/rightpath/admin/homepage" element={<AdminPageHome/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/rightpath/admin' element={<AdminLoginPage />} />
+        <Route path='/rightpath/admin/homepage' element={<AdminHomePage />} />
+      </Routes>
     </BrowserRouter>
   )
 }
 function App() {
-  return (
-  <Main/>
-  );
+  return <Main />
 }
 
-export default App;
+export default App
