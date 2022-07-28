23fdd96696d8b7f1d1c7c57c7b56008a5aadfef1
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-07-28 21:56:29 +0545

    feat: private route for admin homepage

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import Home from './container/Home'
 import AdminLoginPage from './container/Admin/Login/Login'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import AdminHomePage from './container/Admin/Home/Home'
+import PrivateRoute from './utils/PrivateRoutes'
 
 function Main() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/rightpath/admin' element={<AdminLoginPage />} />
-        <Route path='/rightpath/admin/homepage' element={<AdminHomePage />} />
+        <Route
+          exact
+          path='/rightpath/admin'
+          element={
+            <PrivateRoute>
+              <AdminHomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route path='/rightpath/admin/login' element={<AdminLoginPage />} />
       </Routes>
     </BrowserRouter>
   )
 }
 function App() {
-  return <Main />
+  return (
+    <div className='app'>
+      <Main />
+    </div>
+  )
 }
 
 export default App
