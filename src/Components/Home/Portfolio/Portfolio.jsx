afd18900e44e31762e3bfa815115bdc538affb64
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-08-01 21:28:05 +0545

    feat: added portfolio component

---
+++ src/Components/Home/Portfolio/Portfolio.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import './Portfolio.css'
+
+const Portfolio = () => {
+    return (
+        <div className="portfolio">
+            <div className="container">
+                <div className="item1">
+                    <span className="works-icons">
+                    <span className="icon"><i aria-hidden="true" className="fa fa-edit"></i></span>
+                    </span>
+                    <h6 className="works-heading">Counselling Service</h6>
+                    <p className="works-text">Itâ€™s a preliminary service offered by us</p>
+                </div>
+                <div className="item2">
+                    <span className="works-icons">
+                    <span className="icon"><i aria-hidden="true" className="fa fa-clipboard"></i></span>
+                    </span>
+                    <h6 className="works-heading">2. Assessing the documents</h6>
+                    <p className="works-text">We analyse the documents and provide the best solution  </p>
+                </div>
+                <div className="item3">
+                    <span className="works-icons">
+                    <span className="icon"><i aria-hidden="true" className="fa fa-book"></i></span>
+                    </span>
+                    <h6 className="works-heading">3. Visa documentation</h6>
+                    <p className="works-text">We go through the documents are present them as required by the high commission. </p>
+                </div>
+            </div>
+            <div className="our-portfolio">
+                <h4>Our Portfolio.</h4>
+                <span>A creative agency that believes in the power of creative ideas and great design.</span>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio;
