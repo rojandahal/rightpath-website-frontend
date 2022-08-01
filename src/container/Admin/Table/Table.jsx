db64b4848a7bba33b4d977c7524fbc765f09ae9a
rojandahal <maxpanye60@gmail.com>
2022-08-01 22:20:22 +0545

    Table Column Name changed

--- src/container/Admin/Table/Table.jsx
+++ src/container/Admin/Table/Table.jsx
@@ -6,91 +6,85 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { useQuery } from "react-query";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { DOCUMENTFETCH } from "../../Constants/ApiConstants";
 
 const Tables = () => {
-    const rows = [
-        {
-          id: 1143155,
-          product: "Acer Nitro 5",
-          img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
-          customer: "John Smith",
-          date: "1 March",
-          amount: 785,
-          method: "Cash on Delivery",
-          status: "Approved",
+  const {setError} = useForm();
+  const [data, setData] = useState();
+
+  function useData() {
+    return useQuery(
+      "fetchData",
+      async () => {
+        return await axios.get(DOCUMENTFETCH, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      },
+      {
+        enabled: false,
+        onSuccess: (res) => {
+          setData(res?.data.data);
         },
-        {
-          id: 2235235,
-          product: "Playstation 5",
-          img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
-          customer: "Michael Doe",
-          date: "1 March",
-          amount: 900,
-          method: "Online Payment",
-          status: "Pending",
+        onError: (err) => {
+          if (err.response?.data) {
+            setError("SubmitError", {
+              type: "custom",
+              message: err.response?.data?.error,
+            });
+          }
         },
-        {
-          id: 2342353,
-          product: "Redragon S101",
-          img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
-          customer: "John Smith",
-          date: "1 March",
-          amount: 35,
-          method: "Cash on Delivery",
-          status: "Pending",
-        },
-        {
-          id: 2357741,
-          product: "Razer Blade 15",
-          img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
-          customer: "Jane Smith",
-          date: "1 March",
-          amount: 920,
-          method: "Online",
-          status: "Approved",
-        },
-        {
-          id: 2342355,
-          product: "ASUS ROG Strix",
-          img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
-          customer: "Harold Carol",
-          date: "1 March",
-          amount: 2000,
-          method: "Online",
-          status: "Pending",
-        },
-      ];
+        retry: 0,
+      }
+    );
+  }
+
+  const {refetch, isLoading} =  useData();
+
+  useEffect(()=> {
+    refetch();
+    console.log(data==null ? "Null" : "NOT")
+  },[data]);
+
+    const rows = [];
       return (
         <TableContainer component={Paper} className="table">
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell className="tableCell">Tracking ID</TableCell>
-                <TableCell className="tableCell">Product</TableCell>
-                <TableCell className="tableCell">Customer</TableCell>
-                <TableCell className="tableCell">Date</TableCell>
-                <TableCell className="tableCell">Amount</TableCell>
-                <TableCell className="tableCell">Payment Method</TableCell>
-                <TableCell className="tableCell">Status</TableCell>
+                <TableCell className="tableCell">Name</TableCell>
+                <TableCell className="tableCell">Age</TableCell>
+                <TableCell className="tableCell">Contact</TableCell>
+                <TableCell className="tableCell">Email</TableCell>
+                <TableCell className="tableCell">Address</TableCell>
+                <TableCell className="tableCell">Passport No</TableCell>
+                <TableCell className="tableCell">Level Of Study</TableCell>
+                <TableCell className="tableCell">Desired Country</TableCell>
+                <TableCell className="tableCell">Desired Course</TableCell>
+                <TableCell className="tableCell">IELTS/PTE</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
+              {data==null ?
+                <TableRow key="Name">
+                </TableRow>
+               : data.map((row) => (
                 <TableRow key={row.id}>
-                  <TableCell className="tableCell">{row.id}</TableCell>
-                  <TableCell className="tableCell">
-                    <div className="cellWrapper">
-                      <img src={row.img} alt="" className="image" />
-                      {row.product}
-                    </div>
-                  </TableCell>
-                  <TableCell className="tableCell">{row.customer}</TableCell>
-                  <TableCell className="tableCell">{row.date}</TableCell>
-                  <TableCell className="tableCell">{row.amount}</TableCell>
-                  <TableCell className="tableCell">{row.method}</TableCell>
-                  <TableCell className="tableCell">
-                    <span className={`status ${row.status}`}>{row.status}</span>
-                  </TableCell>
+                  <TableCell className="tableCell">{row.name}</TableCell>
+                  <TableCell className="tableCell">{row.age}</TableCell>
+                  <TableCell className="tableCell">{row.contactNumber}</TableCell>
+                  <TableCell className="tableCell">{row.email}</TableCell>
+                  <TableCell className="tableCell">{row.address}</TableCell>
+                  <TableCell className="tableCell">{row.passportNo}</TableCell>
+                  <TableCell className="tableCell">{row.levelStudy}</TableCell>
+                  <TableCell className="tableCell">{row.desiredCountry}</TableCell>
+                  <TableCell className="tableCell">{row.desiredCourse}</TableCell>
+                  <TableCell className="tableCell">{row.courseReq}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
--- src/container/Constants/ApiConstants.js
+++ src/container/Constants/ApiConstants.js
@@ -3,3 +3,5 @@ const API = 'https://rightpathapi.herokuapp.com/api'
 const VERSION = '/v1'
 
 export const ADMINLOGINAPI = API + VERSION + '/auth/login'
+
+export const DOCUMENTFETCH = API + VERSION + '/document'
