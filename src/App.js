cdf82ba50bab7a3a254a556d8d233b04b6935f8f
rojandahal <maxpanye60@gmail.com>
2022-08-03 22:33:01 +0545

    Added Form Route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AdminLoginPage from './container/Admin/Login/Login'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import AdminHomePage from './container/Admin/Home/Home'
 import PrivateRoute from './utils/PrivateRoutes'
+import Form from './container/Form/Form'
 
 function Main() {
   return (
@@ -21,6 +22,7 @@ function Main() {
           }
         />
         <Route path='/rightpath/admin/login' element={<AdminLoginPage />} />
+        <Route exact path='/apply-form' element={<Form/>} />
       </Routes>
     </BrowserRouter>
   )
