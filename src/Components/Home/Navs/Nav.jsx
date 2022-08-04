e0c0a7489b08f6784ad8dec365f146d6436e4ae4
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-08-05 00:40:07 +0545

    fix: block re attachment of url

--- src/Components/Home/Navs/Nav.jsx
+++ src/Components/Home/Navs/Nav.jsx
@@ -24,12 +24,12 @@ const NavBar = ({scrollPosition}) => {
             </div>
             <div className="navigations">
                 <ul>
-                    <li><Link className={`${(scrollPosition < 600 || scrollPosition === undefined) && 'active'}`} to='#' onClick={() => scrollToSection('app')}>Home</Link></li>
+                    <li><Link className={`${(scrollPosition < 600 || scrollPosition === undefined) && 'active'}`} to='/' onClick={() => scrollToSection('app')}>Home</Link></li>
                     <li><Link to='#' className={`${scrollPosition >= 600 && scrollPosition < 1035 && 'active'}`} onClick={() => scrollToSection('working-process')}>Services</Link></li>
                     <li><Link to='#' className={`${scrollPosition >= 1035 && scrollPosition < 1660 && 'active'}`} onClick={() => scrollToSection('portfolio')}>Works</Link></li>
                     <li><Link to='#' className={`${scrollPosition >= 1660 && scrollPosition < 2130 && 'active'}`} onClick={() => scrollToSection('about-us')}>About</Link></li>
                     <li><Link to='#' className={`${scrollPosition >= 2130 && scrollPosition < 2600 && 'active'}`}  onClick={() => scrollToSection('testimonial-view')}>Testimonial</Link></li>
-                    <li><Link to='apply-form' className={`${scrollPosition > 2600 && 'active'}`}>Contact</Link></li>
+                    <li><Link to='/apply-form' className={`${scrollPosition > 2600 && 'active'}`}>Contact</Link></li>
                 </ul>
             </div>
             <div className="mobile-only">
