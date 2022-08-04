288c18bf72c1a99aee4c6fdfd0ba3e56c3675646
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-08-05 00:01:57 +0545

    feat: added scrollPosition to track the scroll

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
 import NavBar from '../Components/Home/Navs/Nav';
 import Greetings from '../Components/Home/Greetings/Greetings';
@@ -8,11 +8,25 @@ import AboutUs from '../Components/Home/Aboutus/AboutUs';
 import GetInTouch from '../Components/Home/GetInTouch/GetInTouch';
 import Footer from '../Components/Home/Footer/Footer';
 
+
 const Home = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
     return (
         <div className='home'>
+          {scrollPosition > 60 && <NavBar scrollPosition={scrollPosition}/>}
           <div className="header-wrapper">
-            <NavBar />
+            <NavBar/>
             <Greetings />
           </div>
           <div className="blank-line"></div>
