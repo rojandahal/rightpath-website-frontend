059ea10187c4e880b26d789b411b8f13868d304a
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-08-05 01:14:09 +0545

    refactor: used constants

--- src/utils/PrivateRoutes.js
+++ src/utils/PrivateRoutes.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Route, Navigate, Routes } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+import { ADMINLOGIN } from '../container/Constants/RoutesConstants'
 import { decryptObjData } from './EncryptDecrypt'
 
 const PrivateRoute = ({ children }) => {
   let userData = decryptObjData(localStorage.getItem('user'))
   const isAuthenticated = userData ? true : false
-  return isAuthenticated ? children : <Navigate to='/rightpath/admin/login' />
+  return isAuthenticated ? children : <Navigate to={ADMINLOGIN} />
 }
 
 export default PrivateRoute
