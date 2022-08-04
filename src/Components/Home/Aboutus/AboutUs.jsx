384ace805fa8a89835276fda5e01a51da2425359
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-08-04 12:30:03 +0545

    feat: added testimonial

--- src/Components/Home/Aboutus/AboutUs.jsx
+++ src/Components/Home/Aboutus/AboutUs.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './AboutUs.css'
 
 const AboutUs = () => {
+    const [testimonial, setTestimonial] = useState([{
+        imgSrc:'//1.bp.blogspot.com/-P1UCUBWGCMU/WaUv1CxF6kI/AAAAAAAADvM/aOSCRX-UHAwKF8EFs20fqryoqDNa3DJQwCK4BGAYYCw/s1600/team_1.jpg',
+        title: 'Rojan Dahal sucess visa granted.',
+        description: 'Date & Details Visa Received: 21st Nov 2019 Difficulties in our life do not come to ruin us,but it helps us to bring our hidden abilities outside. Congratulations Mr Srijan KC for study visa granted to study in Australia.Best Wishes from #KIEC Course Details...',
+    }, {
+        imgSrc:'https://www.kiecglobal.com.au/wp-content/uploads/2020/07/Srijan-KC.jpg',
+        title: 'Saman Shrestha sucess visa granted.',
+        description: 'Date & Details Visa Received: 21st Nov 2019 Difficulties in our life do not come to ruin us,but it helps us to bring our hidden abilities outside. Congratulations Mr Srijan KC for study visa granted to study in Australia.Best Wishes from #KIEC Course Details...',
+    }])
+
+    const [selectedTestimonial, setSelectedTestimonial] = useState()
+    useEffect(() => {
+        let count = 0;
+        setSelectedTestimonial(testimonial[0])
+        count++;
+        function cycleArray() {
+            setSelectedTestimonial(testimonial[count]);
+            count++;
+            // reset counter if we reach end of array
+            if (count === testimonial.length) {
+                 count = 0;
+            }
+        }
+        
+        setInterval(cycleArray, 3000);
+    }, [])
     return (
         <div className="about-us">
             <div className="heading">
@@ -17,6 +43,20 @@ const AboutUs = () => {
             <div className="heading">
                     <h4>Testimonial</h4>
                     <p>Our process on creating awesome projects.</p>
+                    {selectedTestimonial && (
+                        <div className="testimonial">
+                        <div className="left-side">
+                            <img alt="" src={selectedTestimonial?.imgSrc} />
+                        </div>
+                        <div className="right-side">
+                            <div className="title"><h5>{selectedTestimonial?.title}</h5></div>
+                            <div className="description">
+                                <p>{selectedTestimonial?.description}</p>
+                                <span className="readMore">Read More...</span>
+                            </div>
+                        </div>
+                    </div>
+                    )}
             </div>      
         </div>
     )
