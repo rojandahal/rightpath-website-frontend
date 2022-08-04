a9408500753dfe4c2dc291a5b731d5323de70abe
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-08-04 12:29:32 +0545

    feat: added transition static

--- src/Components/Home/Greetings/Greetings.jsx
+++ src/Components/Home/Greetings/Greetings.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -8,10 +8,27 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import './Greetings.css'
 
 const Greetings = () => {
+    const [featureTextArray, setFeatureTextArray] = useState(['We offer complete consultant service.', 'We are Educational Consultant', 'Hello from RightPath.']);
+    const [selectedFeatureText, setSelectedFeatureText] = useState()
+    useEffect(() => {
+        let count = 0;
+        setSelectedFeatureText(featureTextArray[0])
+        count++;
+        function cycleArray() {
+            setSelectedFeatureText(featureTextArray[count]);
+            count++;
+            // reset counter if we reach end of array
+            if (count === featureTextArray.length) {
+                 count = 0;
+            }
+        }
+        
+        setInterval(cycleArray, 3000);
+    }, [])
     return (
         <div className="greetings">
             <h1>Welcome</h1>
-            <p>We are Educational Consultant</p>
+            <p>{selectedFeatureText}</p>
             <div className="social">
                 <ul>
                     <li><FacebookOutlinedIcon/></li>
